105a8272677092730646cda2ff71e37921eab238
AlexSunner <>
2023-11-22 14:41:36 +0000

    Fix issue with cards not duplicate

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,50 @@
 let card = document.querySelectorAll('.card');
 let frontCard = document.querySelectorAll('.front-card');
-let gameArea = document.querySelectorAll('.game-area');
-let score = document.querySelectorAll('.score');
+let gameArea = document.querySelector('.game-area');
+let score = document.querySelector('.score span');
 
 shuffleImage();
 clicking();
+
 function shuffleImage() {
+    console.log(card);
         const num = [...Array(card.length).keys()];
+        console.log('Before shuffle:', num);
         for (let i = num.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [num[i], num[j]] = [num[j], num[i]];
-        }
+        };
+        console.log('After Shuffle:', num);
         card.forEach((c, index) => {
             c.style.order = num[index];
+            console.log(`Card ${index + 1} order: ${c.style.order}`);
         });
 };
 
 function clicking() {
-    for(let i = 0; i<card.length; i++) {
-        frontCard[i].classList.add('show');
-        setTimeout(() => {
-            frontCard[i].classList.remove('show');
-        }, 2000);
-        card[i].addEventListener('click', ()=> {
-            frontCard[i].classList.add('flip');
-            let flippedCard = document.querySelectorAll('.flip');
-            if(flippedCard.length == 2) {
-                gameArea.style.pointerEvents = 'none';
-                setTimeout(() => {
-                    gameArea.style.pointerEvents = 'all';
-                }, 1000);
-                match(flippedCard[0], flippedCard[1]);
-            }
-        })
+    for (let i = 0; i < card.length; i++) {
+        (function (currentIndex) {
+            frontCard[currentIndex].classList.add('show');
+            setTimeout(() => {
+                frontCard[currentIndex].classList.remove('show');
+            }, 2000);
+            card[currentIndex].addEventListener('click', () => {
+                frontCard[currentIndex].classList.add('flip');
+                let flippedCard = document.querySelectorAll('.flip');
+                if (flippedCard.length === 2) {
+                    gameArea.style.pointerEvents = 'none';
+                    setTimeout(() => {
+                        gameArea.style.pointerEvents = 'all';
+                    }, 1000);
+                    match(flippedCard[0], flippedCard[1]);
+                }
+            });
+        })(i);
     }
-};
+}
 
 function match(cardOne, cardTwo) {
-    if (cardOne.dataset.index == cardTwo.dataset.index) {
+    if (cardOne.dataset.index === cardTwo.dataset.index) {
 
         score.innerHTML = parseInt(score.innerHTML) + 1;
 
